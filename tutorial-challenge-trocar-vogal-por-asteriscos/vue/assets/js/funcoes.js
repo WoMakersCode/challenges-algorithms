8e63b8d644f662ed79dafae1e22facfc486888fc
ieleny <ielenyfilgueira@hotmail.com>
2020-10-23 17:29:34 -0300

    Criado a pagina de função

---
+++ tutorial-challenge-trocar-vogal-por-asteriscos/vue/assets/js/funcoes.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function () {
+    new Vue({
+    
+        el:'#app',
+    
+        data: {
+            form: {
+                palavra: null
+            }
+        },
+
+        methods: {
+            trocarVogaisPorAsteriscos(){
+                
+            }
+        }
+    })
+    
+})
